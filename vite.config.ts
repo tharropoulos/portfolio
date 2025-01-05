074af0137486f81761ff342522dd5dfc2ec67401
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-01-05 18:39:01 +0200

    fix: fix postcss for shadcn in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import path from "path";
-
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
 });
