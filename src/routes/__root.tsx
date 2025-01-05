0e57159fcc500158df64d700484fc0ba66471761
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-01-05 21:03:52 +0200

    fix: remove unused import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { Navbar } from "@/components/navbar";
 
