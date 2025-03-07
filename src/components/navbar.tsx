faa1ead8942b8f9c3c938a894d5914189202816f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-03-08 01:56:13 +0200

    feat: add general navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,45 +1,25 @@
+import { Link } from "@tanstack/react-router";
+
 import { ModeToggle } from "@/components/mode-toggle";
-import { NavigationItem } from "@/components/navigation-item";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { NavigationLink, useNavigation } from "@/hooks/useNavigation";
 import { cn } from "@/lib/utils";
 
 export interface NavbarProps {
-  links: NavigationLink[];
   className?: string;
-  scrollOffset?: number;
 }
 
-export function Navbar({ links, className, scrollOffset }: NavbarProps) {
-  const { activeLink, scrollToSection } = useNavigation({
-    links,
-    scrollOffset,
-  });
-
+export function Navbar({ className }: NavbarProps) {
   return (
     <header
       className={cn(
-        "fixed top-0 left-0 right-0 z-50 py-3 bg-gradient-to-b backdrop-blur-sm saturate-200 from-background via-background via-35% to-background/5 border-b border-muted",
+        "fixed left-0 right-0 top-0 z-50 border-b border-muted bg-gradient-to-b from-background via-background via-35% to-background/5 py-3 saturate-200 backdrop-blur-lg",
         className,
       )}
     >
-      <div className="flex justify-between items-center mx-auto max-w-screen-sm px-4 sm:px-6">
-        <h1 className="font-logo">fanis</h1>
-        <NavigationMenu className="px-6 gap-1">
-          <NavigationMenuList>
-            {links.map((link) => (
-              <NavigationItem
-                key={link.id}
-                link={link}
-                isActive={activeLink === link.id}
-                onClick={scrollToSection}
-              />
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
+      <div className="mx-auto flex max-w-screen-sm items-center justify-between px-4 sm:px-6">
+        <Link to="/" className="font-logo">
+          fanis
+        </Link>
+
         <ModeToggle className="hidden md:flex" />
       </div>
     </header>
