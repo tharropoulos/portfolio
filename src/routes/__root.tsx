12fda7fa63e6a7a202fd5e9d587c50fd6f9b6d55
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-03-08 14:17:36 +0200

    refactor: add the navbar on the root component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,35 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
+import { Navbar } from "@/components/main-nav";
+import ScrollProgressBar from "@/components/scrollbar";
+
 export const Route = createRootRoute({
   component: () => (
     <div className="flex min-h-screen w-full flex-col items-center bg-background">
+      <Navbar
+        links={[
+          {
+            id: "about",
+            href: "#about",
+            label: "About",
+          },
+          {
+            id: "work",
+            href: "#work",
+            label: "Work",
+          },
+          {
+            id: "school",
+            href: "#school",
+            label: "School",
+          },
+          {
+            id: "interests",
+            href: "#interests",
+            label: "Interests",
+          },
+        ]}
+      />
       <Outlet />
     </div>
   ),
