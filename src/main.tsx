fdd1d7097a08bb66a51c18de0300f08b2c9576e2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-25 13:35:24 +0300

    feat(analytics): register analytics in root

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // Import the generated route tree
 import "./index.css";
-
+import { Analytics } from "@vercel/analytics/react"
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -22,6 +22,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
+      <Analytics />
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
       </ThemeProvider>
