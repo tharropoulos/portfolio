94173fa05a898c80c35e11afaa94807b88f0d5cf
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-01-30 20:19:44 +0200

    fix(footer): remove `built with` section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,30 +6,6 @@ function Footer() {
       <div className="flex">
         <div className="mx-auto flex max-w-screen-sm flex-col justify-between gap-6 md:flex-row md:items-center md:gap-0">
           <p>{new Date().getFullYear()} | Fanis Tharropoulos</p>
-          <p className="w-1/2">
-            Built with{" "}
-            <a href="https://vite.dev/" className="font-bold hover:underline">
-              Vite
-            </a>
-            ,{" "}
-            <a
-              href="https://tailwindcss.com/"
-              className="font-bold hover:underline"
-            >
-              Tailwind
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://tanstack.com/router/latest"
-              className="font-bold hover:underline"
-            >
-              Tanstack Router
-            </a>
-            , deployed with{" "}
-            <a href="https://vercel.com/" className="font-bold hover:underline">
-              Vercel
-            </a>
-          </p>
         </div>
         <ModeToggle className="flex px-6 md:hidden" />
       </div>
