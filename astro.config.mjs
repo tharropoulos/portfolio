93d323d6c436571b49ebafc03d03a5f0522e0271
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-03-27 15:23:03 +0200

    build(vercel): keep astro deployment static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import path from "node:path";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
   integrations: [react()],
   vite: {
     resolve: {
