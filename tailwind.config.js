d378990661f82d99ccecd779699564ac188c64ce
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-04-20 19:31:12 +0300

    chore(tailwind): enable typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -99,5 +100,5 @@ export default {
   		}
   	}
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, typography],
 };
