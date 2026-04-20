9edabfc5d805c6fe275d408ddc88b0ca0b015981
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-04-20 19:31:11 +0300

    feat(blog): enable MDX and reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import path from "node:path";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 export default defineConfig({
   output: "static",
-  integrations: [react()],
+  integrations: [mdx(), react()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   vite: {
     resolve: {
       alias: {
